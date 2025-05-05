943ad641653d5e22c5ab09199db304302837d931
kirull1 <kivahnin@gmail.com>
2025-05-05 17:20:03 +0300

    refactor: change imports in "app"

--- frontend/src/app/main.ts
+++ frontend/src/app/main.ts
@@ -2,7 +2,7 @@ import './styles/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '../router'
+import router from './router'
 import { appConfig } from '../shared/config/app.config'
 import { createLogger } from '../shared/lib/logger'
 
--- frontend/src/app/router.ts
+++ frontend/src/app/router.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/pages/home/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/pages/about/index.vue'),
+      component: () => import('@/pages/home'),
     },
   ],
 })
