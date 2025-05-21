5476e7433b3eeb7d617d3421539dabdc37f4ce9c
kirull1 <kivahnin@gmail.com>
2025-05-21 14:39:36 +0300

    chore: create isDev function

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { isDev } from '@/shared/lib/isDev';
 import { RouterView } from 'vue-router'
+
+if (isDev()) {
+  console.log('Running in development mode');
+}
 </script>
 
 <template>
